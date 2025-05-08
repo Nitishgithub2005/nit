47dc7db20c3fab652704ddbe82d14f73ce4760b5
Nitish R Maladakar <121116567+Nitishgithub2005@users.noreply.github.com>
2025-05-08 16:43:11 +0530

    Update BufferBuilderPerformence.java

--- Day4/src/BufferBuilderPerformence.java
+++ Day4/src/BufferBuilderPerformence.java
@@ -1,6 +1,8 @@
 public class BufferBuilderPerformence {
     public static void main(String[] args) {
         //builder is more efficient
+        //string builder is more effienct whenwe have larger manipulation and it is also thread safe.
+        //it can run independently in any "thread" -> the smallest unit of procesing
         long stime =System.currentTimeMillis();
         StringBuffer buffer = new StringBuffer("BIET");
         for(int i=1;i<100000000;i++){
