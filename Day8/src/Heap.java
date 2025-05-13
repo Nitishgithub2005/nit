bf791ea45d7fd33f9246af511a058820bddc2858
Nitish R Maladakar <4bd22cs100@bietdvg.edu>
2025-05-13 16:32:21 +0530

    .

--- Day8/src/Heap.java
+++ Day8/src/Heap.java
@@ -59,6 +59,23 @@ public class Heap {
         }
 
     }
+    public static void heapify(int[] arr, int i,int n){
+        int largest=i;
+        int left=2*i;
+        int right=2*i+1;
+        if(left <= n && arr[i]<arr[left]){
+            largest=left;
+        }
+        if (right <= n && arr[i]<arr[right]){
+            largest=right;
+        }
+        if(largest!=i){
+            int temp=arr[i];
+            arr[i]=arr[largest];
+            arr[largest]=temp;
+            heapify(arr,largest,n);
+        }
+    }
     public static void main(String[] args) {
         Heap maxheap = new Heap();
         maxheap.insert(20);
@@ -66,9 +83,23 @@ public class Heap {
         maxheap.insert(50);
         maxheap.insert(10);
         maxheap.insert(40);
+        System.out.println("Before deletion");
         maxheap.print();
+        System.out.println();
+        System.out.println("After deletion");
         maxheap.delete(50);
         maxheap.print();
+        System.out.println();
+        int[] arr={-1,54,53,55,52,50};
+        int n=5;
+        for (int i = n/2; i >=0 ; i--) {
+            heapify(arr,5,0);
+        }
+        System.out.println("Array after heapify");
+        for (int i = 1; i <=n ; i++) {
+            System.out.print(arr[i]+ " ");
+        }
+        System.out.println();
     }
 
 }
